f34f2e601870d85775e9c72310a2cedd593691a5
Anonymity <myrmidon@protonmail.ch>
2020-01-16 15:41:09 +0100

    Countdown revised version 2.

--- src/Countdown.java
+++ src/Countdown.java
@@ -1,39 +1,24 @@
 import java.util.Timer;
 import java.util.TimerTask;
-import java.io.*;
+public class Countdown {
 
-public class Countdown{
 
-    private static String str = "";
-    static Timer timer = new Timer();
-    static TimerTask task = new TimerTask() {
+    Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
         @Override
         public void run() {
-            if(str.equals("")){
-                System.out.println("Fields empty");
-                System.exit(0);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException tits) {
+                tits.printStackTrace();
             }
+            System.out.println("Too late");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException tits) {
+                tits.printStackTrace();
+            }
+            System.exit(0);
         }
     };
-
-    public void run() {
-        
-        timer.schedule( task, 10*1000 );
-
-        System.out.println( "Input a string within 10 seconds: " );
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader( System.in ) );
-        try {
-            str = in.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        timer.cancel();
-        System.out.println( "you have entered: "+ str );
-
-
-    }
-
-
 }
